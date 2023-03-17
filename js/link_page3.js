9e8f5414f42ed4a515863de0ec0988bba3b7cf1b
jeong0214 <124752825+jeong0214@users.noreply.github.com>
2023-03-17 12:14:32 +0900

    Add files via upload

---
+++ js/link_page3.js
@@ -0,0 +1,4 @@
+const $btn_prev_step2 = document.querySelector(".btn_prev_step2");
+$btn_prev_step2.addEventListener("click", () => {
+  window.location.href = "../html/reservation_page2.html";
+});
