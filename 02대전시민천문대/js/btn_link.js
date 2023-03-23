59865b65b9224663a8cc2a27bfd90cb8519404b0
jeong0214 <124752825+jeong0214@users.noreply.github.com>
2023-03-23 16:16:20 +0900

    Delete 02대전시민천문대 directory

--- 02대전시민천문대/js/btn_link.js
+++
@@ -1,5 +0,0 @@
-// 메인화면 버튼 링크
-const $btn_rs_link = document.querySelector(".btn_reservation");
-$btn_rs_link.addEventListener("click", () => {
-  window.location.href = "../html/reservation_page1.html";
-});
